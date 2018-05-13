581ac05f41b6b20ce1e833ebddfd1f26c5e8db13
Iago Laguna <iagolaguna.83@gmail.com>
2018-05-13 00:45:48 -0300

    basic structure

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './scss/index.scss';
+
+const t = () => console.log("teste arrow function");
+t()
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = function (env, argv) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.s?[ac]ss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+            ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        }
+      ]
+    }, 
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+          }),
+        new HtmlWebPackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html"
+        }),
+        
+      ],
+
+    // Pretty stats
+    stats: { colors: true },
+
+    // Sourcemap config
+    devtool: 'source-map'
+}
+}
