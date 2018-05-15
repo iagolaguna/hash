28cbcf5e9d079b3bdc3a0d1e2568faadb5ab881f
Iago Laguna <iagolaguna.83@gmail.com>
2018-05-15 00:26:55 -0300

    stuff and stuff

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import './scss/index.scss'
 const symbols = ['X', 'O'];
+let me = true;
 
 const randomSymbol = () => {
   return symbols[(Math.floor(Math.random() * (100)) % 2)];
 }
+
 const getOppositeSymbol = (symbol) => {
   return symbol === 'X' ? 'O' : 'X';
 }
+
+const getSymbolColor = (symbol = '') => {
+  return symbol === 'X' ? 'aqua' : symbol === 'O' ? 'green' : 'white';
+}
+
 const squares = [...document.getElementsByClassName('square')];
 const symbol = randomSymbol();
+console.log(`My symbol: ${symbol} and my color ${getSymbolColor(symbol)}`);
 
 squares.forEach(square => {
   square.addEventListener('click', event => {
+    //TODO check se já foi clicado
     const data = event.target.dataset;
-
     console.log(data);
+    event.target.style.backgroundColor = getSymbolColor(me ? symbol : getOppositeSymbol(symbol));
     console.log(symbol);
+    const turn = window.confirm('Turn');
+    if (turn) {
+      me = !me;
+    } else {
+      event.target.style.backgroundColor = getSymbolColor();
+    }
   });
 });
