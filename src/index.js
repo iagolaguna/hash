1cecab406bb25729d5792d943fcb59f50df6796e
Iago Laguna <iagolaguna.83@gmail.com>
2018-05-14 23:48:52 -0300

    initing visual logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './scss/index.scss'
+const symbols = ['X', 'O'];
+
+const randomSymbol = () => {
+  return symbols[(Math.floor(Math.random() * (100)) % 2)];
+}
+const getOppositeSymbol = (symbol) => {
+  return symbol === 'X' ? 'O' : 'X';
+}
+const squares = [...document.getElementsByClassName('square')];
+const symbol = randomSymbol();
+
+squares.forEach(square => {
+  square.addEventListener('click', event => {
+    const data = event.target.dataset;
+
+    console.log(data);
+    console.log(symbol);
+  });
+});
